89eb55093155fb5e45a6120ed94e2b80bec9dfc2
Singhal <Deepak.Singhal@Mattel.com>
2026-04-27 22:49:25 +0530

    Commiting the first_script.sh file

---
+++ Github/first_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "This is my frist shell script"
+
+username="Deepak"
+
+
+Num1=1000
+Num2=2000
+Num3=3000
+Num4=$((1000*2000+3000))
+
+echo "the answer of Num4 is $Num4"
+
+echo "this script is created by $username"
+
+
